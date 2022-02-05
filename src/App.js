e493ff87501514d38e48f5d75354254b2e1a0f93
Raphael Tuishev <raf77786@gmail.com>
2022-02-05 19:52:38 +0300

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { useAppDispatch, useAppSelector } from './hooks/redux'
 import { fetchUsers } from './store/reducers/ActionCreators'
